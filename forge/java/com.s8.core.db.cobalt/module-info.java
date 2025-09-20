f96cf498b0d8b153f8104d92708186d8cc3dc8a7
Qx-blocks <convert.pierre@gmail.com>
2025-09-20 11:05:27 +0200

    Updates in the new MODULE system

--- forge/java/com.s8.core.db.cobalt/module-info.java
+++ forge/java/com.s8.core.db.cobalt/module-info.java
@@ -1,9 +1,10 @@
 /**
  * 
  */
-/**
- * 
- */
+
+import com.s8.meta.modular.S8Module;
+
+@S8Module(target = "S8-core-db-cobalt")
 module com.s8.core.db.cobalt {
 	
 	exports com.s8.core.db.cobalt;
@@ -11,6 +12,7 @@ module com.s8.core.db.cobalt {
 	exports com.s8.core.db.cobalt.store;
 	
 
+	requires transitive com.s8.meta;
 	requires transitive com.s8.base.io.xml;
 	
 	requires transitive com.s8.bohr.io.lithium;
