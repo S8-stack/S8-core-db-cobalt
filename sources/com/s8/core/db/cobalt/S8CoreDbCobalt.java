95ec4078657bdcc0c48b92a818773fd76605d98d
pc <pc@pierres-mac-mini.home>
2025-12-06 17:18:35 +0100

    Update for JAVA25 naming conventions conformity

--- sources/com/s8/core/db/cobalt/S8CoreDbCobalt.java
+++ sources/com/s8/core/db/cobalt/S8CoreDbCobalt.java
@@ -7,7 +7,7 @@ import com.s8.meta.modular.S8Module;
  */
 public class S8CoreDbCobalt {
 
-	public final static S8Module MODULE = new S8Module("S8-core-db-cobalt", params -> {
+	public final static S8Module MODULE = new S8Module("S8-core-db-cobalt", _ -> {
 		//params.setWebSources("S8-core-bohr-atom");
 	});
 }
