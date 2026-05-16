f0f2acba66b03d45121c1904a4151df57ebeaa81
pc <pc@192.168.1.13>
2026-05-16 18:50:55 +0200

    Update module description

--- sources/com/s8/core/db/cobalt/S8CoreDbCobalt.java
+++ sources/com/s8/core/db/cobalt/S8CoreDbCobalt.java
@@ -7,7 +7,7 @@ import com.s8.meta.env.modular.S8Module;
  */
 public class S8CoreDbCobalt {
 
-	public final static S8Module MODULE = new S8Module("S8-core-db-cobalt", _ -> {
+	public final static S8Module MODULE = new S8Module("S8-core-db-cobalt", S8CoreDbCobalt.class, _ -> {
 		//params.setWebSources("S8-core-bohr-atom");
 	});
 }
